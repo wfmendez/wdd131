cd7c9bde7d8a6f4bdbbcf3cdb1fe4feb7ca42b6c
wfmendez <wmendezgarcia@byupathway.edu>
2025-08-09 00:44:18 -0400

    feat: Bom Activity - JavaScript localStorage

--- week05/scripts/bom.js
+++ week05/scripts/bom.js
@@ -2,31 +2,51 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
+// Declare an array named chaptersArray and assign it to the result of getChapterList()
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+  displayList(chapter);
+});
 
 button.addEventListener('click', () => {
-    
-    if (input.value.trim() !== '') {
-        
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
-
-        li.textContent = input.value.trim();
-        deleteButton.textContent = '❌';
-        deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
-
-        li.append(deleteButton);
-        list.append(li);
-
-        deleteButton.addEventListener('click', () => {
-            list.removeChild(li);
-            input.focus();
-        });
-
-        input.value = '';
-        input.focus();
-    } else {
-        
-        alert('Please enter a chapter before adding.');
-        input.focus();
-    }
+  if (input.value.trim() !== '') {
+    displayList(input.value);
+    chaptersArray.push(input.value);
+    setChapterList();
+    input.value = '';
+    input.focus();
+  } else {
+    alert('Please enter a chapter before adding.');
+    input.focus();
+  }
 });
+
+function displayList(item) {
+  let li = document.createElement('li');
+  let deletebutton = document.createElement('button');
+  li.textContent = item;
+  deletebutton.textContent = '❌';
+  deletebutton.classList.add('delete');
+  li.append(deletebutton);
+  list.append(li);
+  deletebutton.addEventListener('click', function () {
+    list.removeChild(li);
+    deleteChapter(li.textContent);
+    input.focus();
+  });
+}
+
+function setChapterList() {
+  localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+  return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+  chapter = chapter.slice(0, chapter.length - 1);
+  chaptersArray = chaptersArray.filter(item => item !== chapter);
+  setChapterList();
+}
